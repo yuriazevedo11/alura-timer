39ff65d1d74fb080af0178c2ff1f0265a4360aa4
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-10-19 21:08:17 -0300

    Adição do app na Traybar

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain, Tray, Menu } = require('electron')
 const store = require('./store')
 
-let aboutWindow = null
+let tray
 
 app.on('ready', () => {
   let mainWindow = new BrowserWindow({
@@ -12,8 +12,23 @@ app.on('ready', () => {
     height: 400
   })
 
+  tray = new Tray(`${__dirname}/app/images/icon-tray.png`)
+
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Cursos' },
+    { label: '', type: 'separator' },
+    { label: 'React Native', type: 'radio', checked: true },
+    { label: 'Flutter', type: 'radio' }
+  ])
+
+  tray.setToolTip('Alura Timer')
+  tray.setContextMenu(contextMenu)
+
   mainWindow.loadURL(`${__dirname}/app/index.html`)
 })
+
+// About window events
+let aboutWindow = null
 
 ipcMain.on('open-about-window', () => {
   if (!aboutWindow) {
@@ -39,6 +54,7 @@ ipcMain.on('close-about-window', () => {
   aboutWindow.close()
 })
 
+// Course events
 ipcMain.on('course-stopped', (event, course, timeStudied) => {
   store.saveCourseData(course, timeStudied)
 })
